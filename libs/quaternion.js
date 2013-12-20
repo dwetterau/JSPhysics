6370bfb93317660c80cff5aa9a40664e5c37cfdf
dwetterau <david.wetterau@gmail.com>
2013-12-19 20:52:55 -0600

    vectors and quaternions implemented, untested

--- libs/quaternion.js
+++ libs/quaternion.js
@@ -1,6 +1,44 @@
-function Quaternion(i, j, k, r) {
+function Quaternion(r, i, j, k) {
+  this.r = r;
   this.i = i;
   this.j = j;
   this.k = k;
-  this.r = r;
 }
+
+Quaternion.prototype.normalize = function() {
+  var d = this.r * this.r + this.i * this.i + this.j * this.j + this.k * this.k;
+  if (d == 0) {
+    this.r = 1;
+    return;
+  }
+  d = 1 / Math.sqrt(d);
+  this.r *= d;
+  this.i *= d;
+  this.j *= d;
+  this.k *= d;
+};
+
+Quaternion.prototype.multiply = function(m) {
+  var r = this.r * m.r - this.i * m.i - this.j * m.j - this.k * m.k;
+  var i = this.r * m.i + this.i * m.r + this.j * m.k - this.k * m.j;
+  var j = this.r * m.j + this.j * m.r + this.k * m.i - this.i * m.k;
+  var k = this.r * m.k + this.k * m.r + this.i * m.j - this.j * m.i;
+  this.r = r;
+  this.i = i;
+  this.j = j;
+  this.k = k;
+};
+
+Quaternion.prototype.rotateByVector = function(v) {
+  var q = new Quaternion(0, v.x, v.y, v.z);
+  this.multiply(q);
+};
+
+Quaternion.prototype.addScaledVector = function(v, scale) {
+  var q = new Quaternion(0, v.x * scale, v.y * scale, v.z * scale);
+  q.multiply(this);
+  this.r += q.r * .5;
+  this.i += q.i * .5;
+  this.j += q.j * .5;
+  this.k += q.k * .5;
+};
--- libs/vector.js
+++ libs/vector.js
@@ -3,3 +3,81 @@ function Vector3(x, y, z) {
   this.y = y;
   this.z = z;
 }
+
+Vector3.prototype.invert = function() {
+  this.x = -this.x;
+  this.y = -this.y;
+  this.z = -this.z;
+};
+
+Vector3.prototype.magnitudeSq = function() {
+  return this.x * this.x + this.y * this.y + this.z * this.z;
+};
+
+Vector3.prototype.magnitude = function() {
+  return Math.sqrt(this.magnitudeSq());
+};
+
+Vector3.prototype.normalize = function() {
+  var l = this.magnitude();
+  if (l > 0) {
+    this.scale(1 / l);
+  }
+};
+
+Vector3.prototype.scale = function(scale) {
+  return new Vector3(this.x * scale, this.y * scale, this.z * scale);
+};
+
+Vector3.prototype.scaleInPlace = function(scale) {
+  this.x *= scale;
+  this.y *= scale;
+  this.z *= scale;
+};
+
+Vector3.prototype.add = function(v) {
+  return new Vector3(this.x + v.x, this.y + v.y, this.z + v.z);
+};
+
+Vector3.prototype.addInPlace = function(v) {
+  this.x += v.x;
+  this.y += v.y;
+  this.z += v.z;
+};
+
+Vector3.prototype.sub = function(v) {
+  return new Vector3(this.x - v.x, this.y - v.y, this.z - v.z);
+};
+
+Vector3.prototype.subInPlace = function(v) {
+  this.x -= v.x;
+  this.y -= v.y;
+  this.z -= v.z;
+};
+
+Vector3.prototype.addScaledVector = function(v, scale) {
+  this.x += v.x * scale;
+  this.y += v.y * scale;
+  this.z += v.z * scale;
+};
+
+Vector3.prototype.componentProduct = function(v) {
+  return new Vector3(this.x * v.x, this.y * v.y, this.z * v.z);
+};
+
+Vector3.prototype.componentProductUpdate = function(v) {
+  this.x *= v.x;
+  this.y *= v.y;
+  this.z *= v.z;
+};
+
+Vector3.prototype.dot = function(v) {
+  return this.x * v.x + this.y * v.y + this.z * v.z;
+};
+
+Vector3.prototype.cross = function(v) {
+  return new Vector3(
+      this.y * v.z - this.z * v.y,
+      this.z * v.x - this.x * v.z,
+      this.x * v.y - this.y * v.x);
+};
